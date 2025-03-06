5acf62ebd48406478a866094b1c6552ac741ceea
manupadillaph <manuepad@gmail.com>
2025-03-05 22:35:52 -0300

    delete test sc promt

--- tests/Tests/UnitTests/Main.hs
+++ tests/Tests/UnitTests/Main.hs
@@ -16,6 +16,8 @@ import           Prelude
 import qualified Test.Tasty                           as Tasty
 import qualified Test.Tasty.Options                   as TastyOptions
 import qualified Test.Tasty.QuickCheck                as TastyQC
+import qualified System.Directory as SystemDirectory (doesDirectoryExist, removeDirectoryRecursive)
+import qualified System.IO as SystemIO (hFlush, stdout)
 
 -- Project imports
 import           Contracts.Fund.Holding.MintingPolicy
@@ -37,6 +39,22 @@ import           TestUtils.HelpersMAYZ
 
 main :: IO ()
 main = do
+    putStrLn "---------------"
+
+    -- Prompt user to delete previous test contracts
+    putStr "Do you want to delete previous exported test smart contracts? (yes/no): "
+    SystemIO.hFlush SystemIO.stdout  -- Ensure the prompt is displayed before user input
+    response <- getLine
+    let exportFolder = "export/test/"
+    case response of
+        "yes" -> do
+            exists <- SystemDirectory.doesDirectoryExist exportFolder
+            if exists 
+                then do
+                    SystemDirectory.removeDirectoryRecursive exportFolder
+                    putStrLn "Previous test contracts deleted."
+                else putStrLn "No previous test contracts found."
+        _ -> putStrLn "Keeping previous test contracts."
     putStrLn "---------------"
     tp <- getTestParams "export/test/deploy.json"
     numTests <- getNumTestCases 100
